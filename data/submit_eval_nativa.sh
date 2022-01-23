2f0e3550af1c8261a1d99460913786362a8ee673
Arsen <arsenstakhiv15@gmail.com>
2022-01-23 01:11:08 +0100

    Evolucion nativa

--- data/submit_eval_nativa.sh
+++ data/submit_eval_nativa.sh
@@ -4,12 +4,16 @@
 #SBATCH -p hpc-bio-lejeune
 #SBATCH -J Lab5
 #SBATCH -o RESULTADOS.txt
-#SBATCH -t 0:30:00
 
-time_pyth_13=$(time ./kmer13.py)
-time_pyth_14=$(time ./kmer14.py)
-time_c_13=$(time ./kmer13.cpp)
-time_c_14=$(time ./kmer14.cpp)
-echo "La generacion de todos los posibles 13-mers en Python ha llevado "$time_pyth_13" y en C++ "$time_c_13""
-echo "La generacion de todos los posibles 14-mers en Python ha llevado "$time_pyth_14" y en C++ "$time_c_14""
+echo "La generacion de todos los posibles 13-mers en Python ha llevado:"
+time ./k-mer13.py
+
+echo "La generacion de todos los posibles 14-mers en Python ha llevado:"
+time ./k-mer14.py
+
+echo "La generacion de todos los posibles 13-mers en C++ ha llevado: "
+time ./k-mer13
+
+echo "La generacion de todos los posibles 14-mers en C++ ha llevado: "
+time ./k-mer14
 
